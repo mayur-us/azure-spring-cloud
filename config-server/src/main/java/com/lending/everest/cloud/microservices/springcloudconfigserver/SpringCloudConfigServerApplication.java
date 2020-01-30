d78ed6c319f48cd3635ad133cd5fd8a66da4eb9e
mayur-us <mayur.us@gmail.com>
2020-01-30 18:31:27 +0000

    Changed pom.xml in config-server

--- config-server/src/main/java/com/lending/everest/cloud/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ config-server/src/main/java/com/lending/everest/cloud/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,11 @@ package com.lending.everest.cloud.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 //@SpringBootApplication
-@SpringCloudApplication
+@SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
