8541aff098f66476b4c9a46ba3c3ae255c166963
mayur-us <mayur.us@gmail.com>
2020-01-30 13:01:45 +0000

    Changed in pom.xml

--- limits-service/src/main/java/com/lending/everest/cloud/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/lending/everest/cloud/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,8 +3,10 @@ package com.lending.everest.cloud.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringCloudApplication
 //@EnableHystrix
 public class LimitsServiceApplication {
 	public static void main(String[] args) {
